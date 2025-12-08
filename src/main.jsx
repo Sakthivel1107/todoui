6f1d3c869caf0d88a6cf47ed1a87cac7b52ef150
Sakthivel1107 <svel62628@gmail.com>
2025-12-08 18:44:52 +0530

    fifth commit

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,11 @@ const router = createBrowserRouter([
     path:'Todos',
     element:<Todo/>
   }
-]);
+],
+  {
+    basename: '/todoui',
+  }
+);
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}/>
 )
